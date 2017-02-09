2fbe9eafa779afc4456b0ad2737bbe2134d1fb65
Pop Alexandru Radu <minealex2244@gmail.com>
2017-02-09 11:28:18 +0200

    Update Statusbar.java

--- Statusbar.java
+++ Statusbar.java
@@ -1,19 +1,9 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  
 
-/**
- * Write a description of class Statusbar here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Statusbar extends Actor
 {
-    /**
-     * Act - do whatever the Statusbar wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act() 
     {
-        // Add your action code here.
+        // TODO
     }    
 }
